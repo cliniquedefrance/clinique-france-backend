4765f41e0a80212978e5637d831c726af3d42000
Albert-K <albertk.explorer@gmail.com>
2024-10-30 10:54:54 +0100

    fix: calling unistalled package - morgan

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const morgan = require("morgan");
 const server = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
